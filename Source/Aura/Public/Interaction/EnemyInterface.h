9b5281676fd90838933fae40e02551740a26051f
Nahua Kang <kangnahua@gmail.com>
2023-09-13 17:32:09 +0200

    Generate Basic IEnemyInterface Files for AuraEnemy

---
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -0,0 +1,22 @@
+// Copyright Nahua Kang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UEnemyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class AURA_API IEnemyInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() = 0;
+	virtual void UnhighlightActor() = 0;
+};
