064db83ab0d2decc5b2ecf496b0db28c65591a94
Nahua Kang <kangnahua@gmail.com>
2023-09-15 11:06:47 +0200

    Create and Set Up Basic AuraUserWidget and AuraWidgetController

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Nahua Kang
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Nahua Kang
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
