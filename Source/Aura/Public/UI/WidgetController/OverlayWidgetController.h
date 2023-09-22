84ae78b2bfebbe0dfc664e512a7c18881d6de649
Nahua Kang <kangnahua@gmail.com>
2023-09-22 22:30:05 +0200

    6-10: Broadcast Data Table Rows
    We define `FMessageWidgetRowSignature` delegate type that takes FUIWidgetRow as param.
    We create such a delegate (OverlayWidgetController->MessageWidgetRowDelegate) which will
    be bound when `UOverlayWidgetController::BindCallbacksToDependencies` is called, in which
    we add a lambda callback to `UAuraAbilitySystemComponent->EffectAssetTags` that will then
    use `MessageWidgetRowDelegate` to broadcast the `FUIWidgetRow` which contains tag information.
    
    In Blueprint of WBP_Overlay, on EventWidgetControllerSet, we will bind the MessageWidgetRowDelegate_Event
    to the BP_OverlayWidgetController and break down the Row that's broadcasted and print the message
    of the row.

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -9,12 +9,6 @@
 
 class UAuraUserWidget;
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
-
-
 USTRUCT(BlueprintType)
 struct FUIWidgetRow: public FTableRowBase
 {
@@ -33,6 +27,13 @@ struct FUIWidgetRow: public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	UTexture2D* Image = nullptr;
 };
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 
 
 UCLASS(BlueprintType, Blueprintable)
@@ -55,6 +56,9 @@ public:
 
 	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
 	FOnMaxManaChangedSignature OnMaxManaChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FMessageWidgetRowSignature MessageWidgetRowDelegate;
 
 protected:
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
