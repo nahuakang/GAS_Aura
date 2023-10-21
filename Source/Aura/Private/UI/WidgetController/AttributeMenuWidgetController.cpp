13485bce9a726ef7f3cb147bd5dc6acfa5bf613f
Nahua Kang <kangnahua@gmail.com>
2023-10-21 20:31:00 +0200

    8-15: Create bare minimum AttributeMenuWidgetController

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Nahua Kang
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
