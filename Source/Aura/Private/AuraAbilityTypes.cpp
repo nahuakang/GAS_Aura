da53e30770e8eeef3745be78f1b912f5e6656de1
Nahua Kang <kangnahua@gmail.com>
2023-11-10 14:05:24 +0100

    13-2: Custom GameplayEffectContext -> FAuraGameplayEffectContext

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright Nahua Kang
+
+
+#include "AuraAbilityTypes.h"
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return true;
+}
---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,39 @@
+﻿// Copyright Nahua Kang
+
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	//~Begin FGameplayEffectContext Interface
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+	//~End FGameplayEffectContext Interface
+
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsblockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
