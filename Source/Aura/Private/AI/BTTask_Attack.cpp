6974d604f67b895149888214b1b60fda05184700
Nahua Kang <kangnahua@gmail.com>
2023-11-21 16:44:17 +0100

    14-8: Implement MeleeAttack Behavior Tree

---
+++ Source/Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,9 @@
+// Copyright Nahua Kang
+
+
+#include "AI/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
