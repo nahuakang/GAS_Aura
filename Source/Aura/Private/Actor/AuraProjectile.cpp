0f0267ccb9f45fef818dc03d52d786f2fcca6b88
Nahua Kang <kangnahua@gmail.com>
2023-12-02 16:02:53 +0100

    18-7: Rock Impact Effects

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -51,6 +51,7 @@ void AAuraProjectile::Destroyed()
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if (LoopingSoundComponent)
 			LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 
 	Super::Destroyed();
@@ -70,6 +71,7 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if (LoopingSoundComponent)
 			LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 
 	if (HasAuthority())
